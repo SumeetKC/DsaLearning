ecc70cf8848a6fa984fba16b6bb25eb76d280dde
CHATURVEDI Sumeet <sumeet.chaturvedi@soprabanking.com>
2023-11-08 20:40:35 +0530

    [Basic] : Replaced with enhanced for loop

--- src/main/java/com/basic/MaximumElement.java
+++ src/main/java/com/basic/MaximumElement.java
@@ -7,9 +7,9 @@ public class MaximumElement {
     // Find the maximum element in an array
         int[] arr = {4,6,2,3,9};
         int maximum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > maximum)
-                maximum = arr[i];
+        for (int j : arr) {
+            if (j > maximum)
+                maximum = j;
         }
         System.out.println("Maximum : " + maximum);
         Arrays.stream(arr).forEach(x -> System.out.print(x + " "));
